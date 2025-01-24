e8160d2e94d24ec2a2e9a58432a9a7863691fc70
Emmanuel Yidana <EmmanuelYi@stlghana.com>
2025-01-24 10:23:26 +0000

    paystack

--- src/main/java/com/rent_management_system/RentManagementSystemApplication.java
+++ src/main/java/com/rent_management_system/RentManagementSystemApplication.java
@@ -1,7 +1,5 @@
 package com.rent_management_system;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@OpenAPIDefinition(info = @Info(title = "Rent Management API", version = "v1", description = "API documentation for Rent Management System"))
 public class RentManagementSystemApplication {
 
 	public static void main(String[] args) {
