82db8d1f3a0a34546d47a7ed21847a6f969000c0
Emmanuel Yidana <EmmanuelYi@stlghana.com>
2025-01-24 10:30:48 +0000

    paystack

--- src/main/java/com/rent_management_system/RentManagementSystemApplication.java
+++ src/main/java/com/rent_management_system/RentManagementSystemApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 @EnableJpaAuditing
 public class RentManagementSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RentManagementSystemApplication.class, args);
 	}
